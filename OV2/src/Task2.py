6526b9feac99f562f03fbc3391d31f2d002e4f0d
Nikolai L. Børseth <nikolailb1999@gmail.com>
2022-10-04 11:01:17 +0200

    Finished up the last queries

--- OV2/src/Task2.py
+++ OV2/src/Task2.py
@@ -13,7 +13,7 @@ class Task2:
         self.questions = [
             self.question_1, self.question_2, self.question_3,
             self.question_4, self.question_5, self.question_6,
-            self.question_7, #self.question_8, self.question_9,
+            self.question_7, self.question_8, self.question_9,
             self.question_10, self.question_11,
         ]
     
@@ -36,16 +36,16 @@ class Task2:
         print(f"The average user has {next(iter(average), None)} activities")
         
     def question_3(self):
-        users = pd.DataFrame(self.db_controller.execute_query(
+        users = self.db_controller.select_dataframe(
             """
-            SELECT user_id, COUNT(*) AS c
+            SELECT user_id, COUNT(*) AS num_activities
             FROM activity
             GROUP BY user_id
-            ORDER BY c DESC
+            ORDER BY num_activities DESC
             LIMIT 20
             """
-        ), columns=["user_id", "num_activities"])
-        
+        )
+        users.index += 1
         print("Top 20 user by number of activities:")
         print(tabulate(users, headers="keys", tablefmt="psql"))
         
@@ -60,18 +60,18 @@ class Task2:
         print(f"Users who have used a taxi: {np.array(users).flatten()}")
         
     def question_5(self):
-        transportation_modes = pd.DataFrame(self.db_controller.execute_query(
+        transportation_modes = self.db_controller.select_dataframe(
         """
-            SELECT transportation_mode, COUNT(transportation_mode) AS C
+            SELECT transportation_mode, COUNT(transportation_mode) AS activity_count
             FROM activity
             WHERE transportation_mode IS NOT NULL
             GROUP BY transportation_mode
-            ORDER BY C DESC;
+            ORDER BY activity_count DESC;
         """
-        ), columns=["transportation_mode", "activity_count"])
+        )
         
         print("Instaces of each type of transportation:")
-        print(tabulate(transportation_modes, headers="keys", tablefmt="psql"))
+        print(tabulate(transportation_modes, headers="keys", tablefmt="psql", showindex=False))
     
     def question_6(self):
         query = \
@@ -112,17 +112,48 @@ class Task2:
         print(f"User '112' walked {res:.3f}km in 2008")
         
     def question_8(self):
-        pass
+        print("This might take a while...")
+        feet_to_meter = 0.3048
+        t = self.db_controller.select_dataframe(
+            f"""
+            SELECT activity.user_id, SUM(_altitude_gained) * {feet_to_meter} AS altitude_gained
+            FROM (
+                SELECT track_point.activity_id AS activity_id, GREATEST(track_point.altitude - LAG(track_point.altitude) OVER (ORDER BY track_point.id), 0) AS _altitude_gained
+                FROM track_point
+                WHERE track_point.altitude > 0
+            ) AS t LEFT JOIN activity on (t.activity_id = activity.id)
+            GROUP BY activity.user_id
+            ORDER BY altitude_gained DESC
+            LIMIT 20
+            """)
+        t.index += 1
+        print("Altitude gained per user, top 20:")
+        print(tabulate(t, headers="keys", tablefmt="psql", disable_numparse=True))
+        
     
     def question_9(self):
-        pass
+        print("This might take a while...")
+        invalid = self.db_controller.select_dataframe(
+            """
+            SELECT p.user_id, COUNT(DISTINCT p.activity_id) AS invalid_activities
+            FROM (
+                SELECT a.user_id, t.activity_id, TIMESTAMPDIFF(MINUTE, LAG(t.date_time) OVER (PARTITION BY t.activity_id ORDER BY t.id), t.date_time) AS time_diff
+                FROM activity AS a INNER JOIN track_point AS t ON a.id = t.activity_id
+            ) AS p
+            WHERE p.time_diff >= 5
+            GROUP BY p.user_id
+            ORDER BY invalid_activities DESC
+            """
+        )
+        print("Nr of invalid activities per user:")
+        print(tabulate(invalid, headers="keys", tablefmt="psql", showindex=False))
     
     def question_10(self):
         users = np.array(self.db_controller.execute_query(
         """
             SELECT DISTINCT act.user_id
             FROM activity AS act INNER JOIN track_point AS tp ON act.id = tp.activity_id
-            WHERE ROUND(tp.lat, 3) = 39.916 AND ROUND(tp.lon, 3) = 116.397
+            WHERE (ROUND(tp.lat, 3) BETWEEN 39.915 AND 39.917) AND (ROUND(tp.lon, 3) BETWEEN 116.396 AND 116.398)
         """).all()).flatten()
         print(f"These users have tracked an activity in the forbidden city: {users}")
         
@@ -135,17 +166,15 @@ class Task2:
             res.append((index, value_counts.loc[index].idxmax()))
             
         print("Most used transportation mode per user:")
-        print(tabulate(pd.DataFrame(res, columns=["user_id", "transportation_mode"]).sort_values(by=["user_id"]), headers="keys", tablefmt="psql"))
+        print(tabulate(pd.DataFrame(res, columns=["user_id", "transportation_mode"]).sort_values(by=["user_id"]), headers="keys", tablefmt="psql", showindex=False))
         
 def main():
-
-    
-    # 2008-12-11 04:42:14,2008-12-11 05:15:46
     task2 = Task2()
     for index, question in enumerate(task2.questions):
         print(f"\nQuestion {index + 1}")
         question()
-        input("\nPress enter to recieve next question:")
+        if index < len(task2.questions) - 1: input("\nPress enter to recieve the next question:")
     
 if __name__ == "__main__":
     main()
+    
